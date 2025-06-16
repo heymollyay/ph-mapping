b9501daf47fdd6f1d84dcbc890d71b4188bb2bf6
heymollyay <mollycmidkiff@gmail.com>
2025-06-16 16:11:29 -0400

    gui updates

--- src/main.py
+++ src/main.py
@@ -4,39 +4,44 @@ from operator import truediv
 
 import customtkinter
 
-
-#UI elements
-def greeting():
-    now = datetime.datetime.now().time()
-    if datetime.time(0, 0) <= now <= datetime.time(11, 59):
-        greeting = "Good morning, how can I help you?"
-    elif datetime.time(12, 0) <= now <= datetime.time(16, 59):
-        greeting = "Good afternoon, how can I help you?"
-    else:
-        greeting = "Good evening, how can I help you?"
-    return greeting
-
 class Menu(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
+        #make sure to configure grid row/columns for frames as well so they change size
+        self.grid_rowconfigure((0,1,2,3), weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
         self.option_1 = customtkinter.CTkButton(self, text="New Project...")
-        self.option_1.grid(row=0, column=0, padx=10, pady=10, sticky= "ew")
+        self.option_1.grid(row=0, column=0, padx=10, pady=10, sticky=tk.NSEW)
 
         self.option_2 = customtkinter.CTkButton(self, text="Open...")
-        self.option_2.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+        self.option_2.grid(row=1, column=0, padx=10, pady=10, sticky=tk.NSEW)
 
         self.option_3 = customtkinter.CTkButton(self, text="Save as...")
-        self.option_3.grid(row=2, column=0, padx=10, pady=10, sticky="ew")
+        self.option_3.grid(row=2, column=0, padx=10, pady=10, sticky=tk.NSEW)
 
         self.option_4 = customtkinter.CTkButton(self, text="Settings")
-        self.option_4.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
+        self.option_4.grid(row=3, column=0, padx=10, pady=10, sticky=tk.NSEW)
 
-class Workbench(customtkinter.CTkFrame):
+class Workbench(customtkinter.CTkTabview):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.add("New")
+        #Add new tab if new project is created
+        open_greeting = "Welcome to PH Mapping. How can I assist you today?"
+        self.message = customtkinter.CTkLabel(self, text=open_greeting)
+        self.message.grid(row=0, column=0, padx=10, pady=10, sticky=tk.NSEW)
+
+class MapView(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
 
+class DataView(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
 
 
 
@@ -45,15 +50,23 @@ class App(customtkinter.CTk):
         super().__init__()
 
         self.title("Mapping Assistant")
-        self.geometry("720x480")
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.geometry("820x580")
+        self.columnconfigure((0,1,2), weight=1)
+        self.rowconfigure((0,1), weight=1)
+
 
         self.menu = Menu(self)
-        self.menu.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+        self.menu.grid(row=0, column=0, rowspan=2, padx=10, pady=10, sticky=tk.NSEW)
 
         self.workbench = Workbench(self)
-        self.workbench.grid(row=0, column=1, columnspan=2, padx=10, pady=10, sticky=tk.W)
+        self.workbench.grid(row=0, column=1, rowspan=2, padx=10, pady=10, sticky=tk.NSEW)
+
+        self.mapview = MapView(self)
+        self.mapview.grid(row=0, column=2, padx=10, pady=10, sticky=tk.NSEW)
+
+        self.dataview = DataView(self)
+        self.dataview.grid(row=1, column=2, padx=10, pady=10, sticky=tk.NSEW)
+
 
 #title = customtkinter.CTkLabel(app, text = greeting() )
 #title.pack(padx = 5, pady = 5)
